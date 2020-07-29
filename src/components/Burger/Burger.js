30e4b8329a68e866cd0f3fbc1ffbf0f62f9bb31f
Sinatrio Bimo Wahyudi <44100152+sinatriiobimo@users.noreply.github.com>
2020-07-29 11:38:13 +0700

    Setup BurgerIngridient component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+export default function Burger(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+export default class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        )
+    }
+}
